05c107d3a3f5b32d096afc362e673875667dd01d
大島裕二 <yuuji19941109@ooshimayuujinoMacBook-Air.local>
2022-04-03 20:19:41 +0900

    first_commit 宿泊予約アプリの実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,81 @@
+class UsersController < ApplicationController
+  
+  def top
+  end
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_params)
+    if @user.save
+      redirect_to root_path
+      flash[:notice] = "Welcome! You have signed up successfully."
+      session[:user_id] = @user.id
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def create_params
+    params.require(:user).permit(:name, :email, :passward, :password, :password_confirmation, :image)
+  end
+
+  def sign_in_form
+  end
+
+  def sign_in
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:notice] = "Signed in successfully."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "メールアドレスかパスワードが違います"
+      render "sign_in_form"
+    end
+  end
+
+  def log_out
+    session[:user_id] = nil
+    flash[:notice] = "Signed out successfully."
+    redirect_to root_path
+  end
+
+  def acount
+    @user = User.find_by(id:session[:user_id])
+  end
+
+  def edit
+    @user = User.find_by(id:session[:user_id])
+  end
+
+  def update
+    @user = User.find_by(id:session[:user_id])
+    if @user.update(params.require(:user).permit(:password_confirmation, :email, :password, :password_digest))
+      flash[:notice] = "Your account has been updated successfully."
+      redirect_to root_path
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
+  def profile
+    @user = User.find_by(id:session[:user_id])
+  end
+
+  def update_profile
+    @user = User.find_by(id:session[:user_id])
+    if @user.update(params.permit(:name, :introduction, :image))
+      flash[:notice] = "Your account has been updated successfully."
+      redirect_to root_path
+    else
+      render "profile"
+    end
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ReservationApp
+module PoteShare
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -18,5 +18,7 @@ module ReservationApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
   end
 end
